e76f10e75c1d157c90e28f8a2c37f10832665c56
Jose Naime <espinin123@gmail.com>
2018-10-26 15:26:04 -0500

    Frutitas

---
+++ JuegoFrutas/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    [SerializeField]public Color[] colors; // 0 Rojo, 1 verde
+    GameObject leftDoor;
+    GameObject rightDoor;
+
+    
+
+    private void Start()
+    {
+        leftDoor = GameObject.Find("LeftDoor");
+        rightDoor = GameObject.Find("RightDoor");
+        // Color
+        leftDoor.GetComponent<SpriteRenderer>().color = colors[1];
+        rightDoor.GetComponent<SpriteRenderer>().color = colors[0];
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && leftDoor.GetComponent<SpriteRenderer>().color == colors[1]) { 
+        leftDoor.GetComponent<SpriteRenderer>().color = colors[0];
+        rightDoor.GetComponent<SpriteRenderer>().color = colors[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && leftDoor.GetComponent<SpriteRenderer>().color == colors[0])
+        {
+            leftDoor.GetComponent<SpriteRenderer>().color = colors[1];
+            rightDoor.GetComponent<SpriteRenderer>().color = colors[0];
+        }
+
+
+        if (leftDoor == null || rightDoor == null)
+        {
+            ReCheckDoors();
+        }
+    }
+
+    void ReCheckDoors()
+    {
+        leftDoor = GameObject.Find("LeftDoor");
+        rightDoor = GameObject.Find("RightDoor");
+    }
+}
---
+++ JuegoFrutas/Assets/Scripts/LeftDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftDoor : MonoBehaviour {
+    Color[] colors; // 0 Rojo, 1 verde
+    Color color;
+    bool enable;
+    Collider2D col;
+	// Use this for initialization
+	void Start () {
+        colors = DoorScript.FindObjectOfType<DoorScript>().colors;
+        color = gameObject.GetComponent<SpriteRenderer>().color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (color == colors[0])
+        {
+
+        }
+	}
+}
