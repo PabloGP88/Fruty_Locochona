78fc00932540d32497150248ce5a58c29f5475db
Jose Naime <espinin123@gmail.com>
2018-10-26 13:16:40 -0500

    2D

---
+++ Fruty/Assets/Scripts/Bound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bound : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Destroy(col.gameObject);
+        if (col.gameObject.tag == "Healthy"){
+            Debug.Log("Comida Sana");
+            Destroy(col.gameObject);
+        }
+        if (col.gameObject.tag == "NoHealthy")
+        {
+            Debug.Log("Comida Chatarra");
+            Destroy(col.gameObject);
+        }
+    }
+}
