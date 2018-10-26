32a02fd2e147e556948995285d5b3970977308a3
Jose Naime <espinin123@gmail.com>
2018-10-26 17:03:16 -0500

    Menu

---
+++ JuegoFrutas/Assets/Scripts/Scene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void SceneToLoad(string Scene)
+    {
+        SceneManager.LoadScene(Scene);
+    }
+}
