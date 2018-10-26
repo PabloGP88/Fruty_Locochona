92ae196a3838ef1df865803aeb019874a2190350
Jose Naime <espinin123@gmail.com>
2018-10-26 11:42:55 -0500

    1

---
+++ Fruty/Assets/Scripts/FoodSpawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawn : MonoBehaviour {
+    [Header("VariablesGlobales")]
+    public bool inGame;
+    [Header("Food")]
+    public float SpawnTime;
+    int randomSelect;
+    public GameObject[] food;
+
+    
+
+
+    // -----------------------------------------------------
+	void Start () {
+        randomSelect = Random.Range(0, food.Length);
+        inGame = GameManager.FindObjectOfType<GameManager>().inGame;
+	}
+	
+	void Update () {
+
+        StartCoroutine(Spawn());
+	}
+    IEnumerator Spawn()
+    {
+        if (inGame == true)
+        {
+            Instantiate(food[randomSelect]);
+            yield return new WaitForSeconds(SpawnTime);
+        }
+    }
+}
